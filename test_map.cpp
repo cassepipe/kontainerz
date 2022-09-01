96d773d58523f6e4945aed28434e3696b9c7ddca
cassepipe <cassepipe@ymail.com>
2022-09-01 20:41:43 +0200

    Passing map begin

--- test_map.cpp
+++ test_map.cpp
@@ -6,14 +6,14 @@
 
 void test_map()
 {
-	//test_map_begin() ;
+	test_map_begin() ;
 	//test_map_clear() ;
 	//test_map_constructor() ;
 	//test_map_count() ;
 	//test_map_empty() ;
 	//test_map_end() ;
 	//test_map_equal_range() ;
-	test_map_erase() ;
+	//test_map_erase() ;
 	//test_map_find() ;
 	//test_map_get_allocator() ;
 	//test_map_insert() ;
