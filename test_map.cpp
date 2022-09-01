3d7d10345af1a7f6f76a8a6a4f4ab32ad101c61c
cassepipe <cassepipe@ymail.com>
2022-09-01 20:42:46 +0200

    Passsing constructor

--- test_map.cpp
+++ test_map.cpp
@@ -7,8 +7,8 @@
 void test_map()
 {
 	//test_map_begin() ;
-	test_map_clear() ;
-	//test_map_constructor() ;
+	//test_map_clear() ;
+	test_map_constructor() ;
 	//test_map_count() ;
 	//test_map_empty() ;
 	//test_map_end() ;
