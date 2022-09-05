834855c74012610ad41df9d1f46019a6871d0e72
cassepipe <cassepipe@ymail.com>
2022-09-05 12:55:51 +0200

    Passing vector get_allocator test

--- test_vector.cpp
+++ test_vector.cpp
@@ -12,8 +12,8 @@ void test_vector()
 	//test_vector_empty();
 	//test_vector_end();
 	//test_vector_erase();
-	 test_vector_front();
-	 test_vector_get_allocator();
+	test_vector_front();
+	test_vector_get_allocator();
 	// test_vector_insert();
 	// test_vector_max_size();
 	// test_vector_operator_bracket();
@@ -789,6 +789,23 @@ void test_vector_front()
 
 void test_vector_get_allocator()
 {
+	NAMESPACE::vector<int>	myvector;
+	int *	p;
+	unsigned int	i;
+
+	// allocate an array with space for 5 elements using vector's allocator:
+	p = myvector.get_allocator().allocate(5);
+
+	// construct values in-place on the array:
+	for (i=0; i<5; i++) myvector.get_allocator().construct(&p[i],i);
+
+	std::cout << "The allocated array contains:";
+	for (i=0; i<5; i++) std::cout << ' ' << p[i];
+	std::cout << '\n';
+
+	// destroy and deallocate:
+	for (i=0; i<5; i++) myvector.get_allocator().destroy(&p[i]);
+	myvector.get_allocator().deallocate(p,5);
 }
 
 void test_vector_insert()
