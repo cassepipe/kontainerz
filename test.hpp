c1fe677762d4a3b2a8a6971b4dbf0f2e467035d6
cassepipe <cassepipe@ymail.com>
2022-09-05 11:39:13 +0200

    test.h -> test.hpp

--- test.hpp
+++ test.hpp
@@ -1,16 +1,24 @@
 #ifndef TEST_H
 #define TEST_H
 
+#include "map.hpp"
 #include "vector.hpp"
 
-#include <iostream>
-#include <string>
+#include <map>
+#include <stack>
+#include <vector>
 
 #ifndef NAMESPACE
 #define NAMESPACE ft
 #endif
 
+using NAMESPACE::make_pair;
+using NAMESPACE::pair;
 using NAMESPACE::vector;
+// using NAMESPACE::stack;
+using NAMESPACE::map;
+
+using std::cout;
 using std::string;
 
 #endif /* TEST_H */
