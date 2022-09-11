9405fc8720b4e462631bccc59cb72321751d0c7a
cassepipe <cassepipe@ymail.com>
2022-09-11 16:31:26 +0200

    New test

--- test_vector.cpp
+++ test_vector.cpp
@@ -1217,95 +1217,13 @@ void test_vector_swap_overload()
 }
 
 /* DANGER ZONE */
+
+#include "prelude.hpp"
+#include "vector_prelude.hpp"
   
-template <typename ForwardIt, typename T>
-void iota(ForwardIt first, ForwardIt last, T value = T())
-{
-    while (first != last) {
-        *first++ = value;
-        ++value;
-    }
-}
-
-template < typename T >
-void init_array(T* arr, std::size_t size)
-{
-	iota(arr, arr + size, (T)rand());
-}
-
-template < typename T >
-T times2(const T& x)
-{
-	return x * 2;
-}
-
-template < typename It >
-void print_range(It begin, It end)
-{
-	for (; begin != end; ++begin)
-	{
-		cout << *begin;
-	}
-	cout << endl;
-}
-
 void test_vector_assign_range()
 {
-	long s_long[32];
-	init_array(s_long, 32);
-
-	long b_long[64];
-	init_array(b_long, 64);
-
-	int s_int[32];
-	init_array(s_int, 32);
-
-	int b_int[64];
-	init_array(b_int, 64);
-
-	double s_double[32];
-	init_array(s_double, 32);
-
-	double b_double[64];
-	init_array(b_double, 64);
-
-	char s_char[32];
-	iota(s_char, s_char + 32, '@');
-	char b_char[64];
-	iota(b_char, b_char + 64, '$');
-	std::size_t s_size = 32;
-	std::size_t b_size = 64;
-	(void)s_size;
-	(void)b_size;
-	std::string s_string[32] = {
-	    "QExoqp0nICr0sXsHqty2", "naax9QcpJhvaL7DezsNQ", "25ZTtB6wbptfbxM8AvHB", "tShYNtc0MkdjqLrOatgz",
-	    "7Z3kf1Qec0NnsLSEpbOt", "WhkSNrKJC966fvjZ2Or1", "8vlxlFRRgW7yYj4GO7dt", "5sq1aoT8zP0HaHj3nFOK",
-	    "61Dv31GYZhkgjKCErpng", "l0IIcWWHhOcPzoxEamQM", "bE1RYclskwXlhCm46YFs", "kXeoi5qz94JYPqSDTs79",
-	    "TFsQP1dz8VVos9KzUpY0", "b3wYQR7An193gjgYuRj3", "xSmyNsnSJ47jLqrvbpyr", "guAIP2Wq43Gf8VhHsyu5",
-	    "yT6c2loPebHovnq9BQog", "3qvU1xcVm2g1DKFDlqh4", "L0q8RR9P41VD4sVjdnWl", "YdjESsIZM4b1oGQPjpBe",
-	    "l1ZVQbWKw7brHUSimJgq", "xdn0cf4vqRzpfXWtl10G", "lYnZvpqaV0s8DowMZwzV", "8P1cyKrwJNLoJyvLjYqO",
-	    "4MhOXNbAX23CEijO5cRT", "tHe3miAwCOVQbuoLaEP2", "l6uNLsc8fiLl3eWoG6j6", "477xt6l0lph9ALQdr4HX",
-	    "D9UJNe4s8YF02LhrwOdl", "dLCisBNOdE8yugntu6cj", "YvY4aQFHgAuagn4dFLO1", "eGR6Dtv7LW75qlV5Fkik"};
-	std::string b_string[64] = {
-	    "uvg6KqtcgduR31n3ajsv", "wbiAcjgojb9JOagZwyMn", "ATZKCzaPOqgkhPjwuGlf", "MOhaJs56yjOw8f6nLPRA",
-	    "0gyB2Tr42v6awMw2nK7J", "e6GsiLFUuoDpVFEhJKZ1", "z0jXAhiV9keBsaLOY0Xf", "P68p2ZAosHJdmoZh1C7N",
-	    "Pu3EuZVeY0TCO3ojdK0t", "z7jCHMooHCS73M8GygKB", "uT4KoK83JrZxZjkul7ty", "g8gfrZoY5XwfzRusvHvv",
-	    "7PGmkM0OSRnYREt9mFIP", "q1od7mBIpPEsCtpF9kdw", "XQo0LWId5TdZnLnpUNOb", "U0m1R0kFFhAFyS6hmHHw",
-	    "K0lPKfxJxIOnE8QB90xn", "cZ5xyQifMJhrKxqBK9A7", "cFBiwjfYw7Js6qEGy5Kt", "1tW0KWfXxeFO69tByqcE",
-	    "3Fvq9NxBrhPXHe0IlIVx", "MSRDjdFRvHAhFGhiMtDe", "zGm2joMh71jQkYzg5L4V", "Mq4RRaeLvSAO0z2ibp8Q",
-	    "WnLFYnQKP8TNJkqVVbUg", "E98UphbbVSzrW5Mzurmg", "F8HRxeEcaTZDkFPkioij", "jmUVl4Q8X5BwVNzXN219",
-	    "n7Xp4w4FwzGKit7AI4SO", "4MxXYr6rKOcXLt9UkVd2", "4RVTDsADtRyboaai9d29", "XaSqsrrtdhAfFoJIc5KK",
-	    "9Z9jdVCrTT09bg348ceb", "I6uqLG9dO5mfNdSMwOYm", "UwMTzJPlbnhgwbHpDi6w", "DebjMP9afncYE6GhhO00",
-	    "YGPuscYDiGfAjY1UWST0", "K6gbvgGjVZgEFUDlkdSk", "8xCBPI0w6TpC0RA62c2W", "fYMxkNwmKg3moP8KvD9v",
-	    "QpPdhwhEYjIugg3OPcPH", "qQBXjSn43I3EMP54SyxZ", "7qvdKwoW1CQEZTWPvuSC", "rCT212rdYO0zTGHXesKg",
-	    "dBHvlHsBwcR9MkkenYYG", "NQiSlergqR8fVbOeivLj", "xYVqsV147UIe7jVBVwXo", "tcxayO4DdEJ885TbqUMy",
-	    "9TGSMTD8U8ksRpHqq0cL", "TIJ16jCv9BSUiWvhbF9T", "BM9GL2ig1hePkA6lM6Ck", "TfJTYB9JQMU6CGcYg20Q",
-	    "Fg6e5YT2FQbpTZNTDqdo", "LI5q6ml40MeE9H1dPb93", "OaxJUSm3nYN9Y8Ela7sS", "BgBeODAwXz7xJo50Rwqd",
-	    "xdkgKj1dEoJ6zuVhkvvo", "olIewtUEvXJgs1lB9bCn", "dTsPDS0x2uXtcgOIJHb8", "DYvJ2phLppGNZKboTBrd",
-	    "DjNFMtt9PxkzqvWBHI6j", "1Z3YkeTFlPniKnzFhzgu", "76XqQg6hqMf5IXxKPOEs", "gzaapTWW7i9EZjjzLeK6"};
-	(void)s_string;
-	(void)b_string;
-
+	SETUP_ARRAYS()
 	{
 		vector< std::string, std::allocator< std::string > > v;
 		{
@@ -1683,35 +1601,76 @@ void test_vector_comparisons_ge()
     }
 }
 
+void vec_test_erase()
+{
+    SETUP_ARRAYS();
+
+    {
+        intvector v(b_int, b_int + b_size - 5);
+
+        intvector::iterator it = v.erase(v.begin() + 26);
+
+        PRINT_LINE("It:", *it);
+        CHECK_AND_PRINT_ALL(v);
+
+        it = v.erase(it);
+
+        PRINT_LINE("It:", *it);
+        CHECK_AND_PRINT_ALL(v);
+
+        it = v.erase(v.end() - 1);
+
+        PRINT_LINE("It:", *it);
+        CHECK_AND_PRINT_ALL(v);
+
+        it = v.erase(v.begin());
+
+        PRINT_LINE("It:", *it);
+        CHECK_AND_PRINT_ALL(v);
+    }
+
+    {
+        intvector v(1, 5);
+
+        intvector::iterator it = v.erase(v.begin());
+
+        if (it != v.end()) {
+            PRINT_MSG("Wrong iterator");
+        }
+        CHECK_AND_PRINT_ALL(v);
+    }
+}
+
 
 void test_vector()
 {
-	 test_vector_assign();
-	 test_vector_at();
-	 test_vector_back();
-	 test_vector_begin();
-	 test_vector_capacity();
-	 test_vector_clear();
-	 test_vector_constructors();
-	 test_vector_empty();
-	 test_vector_end();
-	 test_vector_erase();
-	 test_vector_front();
-	 test_vector_get_allocator();
-	 test_vector_insert();
-	 test_vector_max_size();
-	 test_vector_operator_bracket();
-	 test_vector_operator_equal();
-	 test_vector_pop_back();
-	 test_vector_push_back();
-	 test_vector_rbegin();
-	 test_vector_relational_operators();
-	 test_vector_rend();
-	 test_vector_reserve();
-	 test_vector_resize();
-	 test_vector_size();
-	 test_vector_swap();
-	 test_vector_swap_overload();
+// 	 test_vector_assign();
+// 	 test_vector_at();
+// 	 test_vector_back();
+// 	 test_vector_begin();
+// 	 test_vector_capacity();
+// 	 test_vector_clear();
+// 	 test_vector_constructors();
+// 	 test_vector_empty();
+// 	 test_vector_end();
+// 	 test_vector_erase();
+// 	 test_vector_front();
+// 	 test_vector_get_allocator();
+// 	 test_vector_insert();
+// 	 test_vector_max_size();
+// 	 test_vector_operator_bracket();
+// 	 test_vector_operator_equal();
+// 	 test_vector_pop_back();
+// 	 test_vector_push_back();
+// 	 test_vector_rbegin();
+// 	 test_vector_relational_operators();
+// 	 test_vector_rend();
+// 	 test_vector_reserve();
+// 	 test_vector_resize();
+// 	 test_vector_size();
+// 	 test_vector_swap();
+// 	 test_vector_swap_overload();
 	 test_vector_assign_range();
 	 test_vector_comparisons_ge();
+ vec_test_erase();
 }
